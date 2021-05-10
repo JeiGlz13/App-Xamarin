be93c0fe5ac4411f830517a2db095371aede4f54
JeiGlz13 <72113139+JeiGlz13@users.noreply.github.com>
2021-05-09 22:33:14 -0600

    Apps 4 y 5 validadas

--- Calculadora/CalculadoraLogaritmo.xaml.cs
+++ Calculadora/CalculadoraLogaritmo.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Transactions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,8 +16,75 @@ namespace Calculadora
         {
             InitializeComponent();
 
-            cbLogaritmo.Items.Add("Factorial");
-            cbLogaritmo.Items.Add("Logaritmo");
+            string[] funciones = { "Factorial", "Logaritmo10", "LogaritmoNatural"};
+            for (int i = 0; i < 3; i++) { cbLogaritmo.Items.Add(funciones[i]); }
+
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n == 0)               
+                return 1;
+            return n * Factorial(n - 1); 
+        }
+
+        private void cbLogaritmo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int valor;
+            string tbValor;
+            double logaritmo = 0;
+            string resultado;
+            string operacion;
+
+            tbValor = tbLogaritmo.Text;
+
+            if(tbValor == ""){
+                DisplayAlert("Hola Usuario", "Ingrese un valor", "OK");
+            }
+            else
+            {
+                char[] test = tbValor.ToCharArray();
+                bool entero = true;
+
+                for (int i = 0; i < test.Length; i++)
+                {
+                    if (test[i] == '.')
+                    {
+                        entero = false;
+                    }
+                }
+                if ((entero == false)||(tbValor == "-"))
+                {
+                    DisplayAlert("Hola Usuario", "Ingrese un numero entero", "OK");
+                }
+                else
+                {
+                    valor = Convert.ToInt32(tbValor);
+                    operacion = cbLogaritmo.SelectedItem.ToString();
+
+                    if (valor < 0) DisplayAlert("Hola Usuario", "Ingrese un numero positivo", "OK");
+                    else
+                    {
+                        switch (operacion)
+                        {
+                            case "Factorial":
+                                logaritmo = Factorial(valor);
+                                break;
+                            case "Logaritmo10":
+                                logaritmo = Math.Log10(valor);
+                                break;
+                            case "LogaritmoNatural":
+                                logaritmo = Math.Log(valor);
+                                break;
+                        }
+
+                        resultado = Convert.ToString(logaritmo);
+                        lblResultado.Text = resultado;
+                    }
+
+                    
+                }
+            }
         }
     }
 }
--- Calculadora/CalculadoraTrigonometrica.xaml.cs
+++ Calculadora/CalculadoraTrigonometrica.xaml.cs
@@ -16,10 +16,77 @@ namespace Calculadora
         {
             InitializeComponent();
 
-            cbTrigonometricas.Items.Add("Seno");
-            cbTrigonometricas.Items.Add("Coseno");
-            cbTrigonometricas.Items.Add("Tangente");
-            cbTrigonometricas.Items.Add("Secante");
+            string[] funciones = { "Seno", "Coseno", "Tangente", "Cosecante", "Secante", "Cotangente" };
+            for (int i = 0; i < 6; i++) { cbTrigonometricas.Items.Add(funciones[i]); }
+           
+        }
+
+        private void cbTrigonometricas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tbValor;
+            double funcion = 0;
+            double funcionRadian = 0;
+            string resultado;
+            string resultadoRadian;
+            string operacion;
+            double valorRadian = 0;
+            double valorGrado = 0;
+
+            tbValor = tbTrigonometrica.Text;
+            if (tbValor == "")
+            {
+                DisplayAlert("Hola usuario", "Rellene el campo", "OK");
+            }
+            else
+            {
+               
+                if ((tbValor == "-") || (tbValor == "."))
+                {
+                    DisplayAlert("Hola usuario", "Ingrese un numero", "OK");
+                }
+                else
+                {
+                    valorRadian = Convert.ToDouble(tbValor);
+                    valorGrado = (valorRadian/180) * Math.PI;
+                    operacion = cbTrigonometricas.SelectedItem.ToString();
+
+                    switch (operacion)
+                    {
+                        case "Seno":
+                            funcion = Math.Sin(valorGrado);
+                            funcionRadian = Math.Sin(valorRadian);
+                            break;
+                        case "Coseno":
+                            funcion = Math.Cos(valorGrado);
+                            funcionRadian = Math.Cos(valorRadian);
+                            break;
+                        case "Tangente":
+                            funcion = Math.Tan(valorGrado);
+                            funcionRadian = Math.Tan(valorRadian);
+                            break;
+                        case "Cosecante":
+                            funcion = (1 / (Math.Sin(valorGrado)));
+                            funcionRadian = (1 / (Math.Sin(valorRadian)));
+                            break;
+                        case "Secante":
+                            funcion = (1 / (Math.Cos(valorGrado)));
+                            funcionRadian = (1 / (Math.Cos(valorRadian)));
+                            break;
+                        case "Cotangente":
+                            funcion = (1 / (Math.Tan(valorGrado)));
+                            funcionRadian = (1 / (Math.Tan(valorRadian)));
+                            break;
+                        default:
+                            DisplayAlert("Hola Usuario", "Ingrese una opcion validad", "OK");
+                            break;
+                    }
+
+                    resultado = Convert.ToString(funcion);
+                    resultadoRadian = Convert.ToString(funcionRadian);
+                    lblResultado.Text = "Grados: " + resultado;
+                    lblResultadoRadian.Text = "Radian: " + resultadoRadian;
+                }
+            }
         }
     }
 }
